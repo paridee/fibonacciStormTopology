f966c9a7416c2133d46b36f6b3210c5c7ea35c78
paride <paride.casulli@gmail.com>
2016-09-07 13:02:07 +0200

    changedtopvalues

--- src/GenericWorkerTopology/BenchmarkTopology.java
+++ src/GenericWorkerTopology/BenchmarkTopology.java
@@ -25,7 +25,7 @@ public class BenchmarkTopology {
 	   	  	int[] intervals	=	new int[24];
 	   	  	
 	   	  	for(int i=0;i<24;i++){
-	   	  		intervals[i]	=	20; //su pc adriano 30
+	   	  		intervals[i]	=	15; //su pc adriano 30
 	   	  	}
 	   	  	
 	   	  	/*
